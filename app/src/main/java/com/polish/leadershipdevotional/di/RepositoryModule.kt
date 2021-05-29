9bdc77f6ee9c0e6bde7dcebe26327315a6e911dd
Osehi <osehiaseehilen@gmail.com>
2021-05-29 13:15:51 +0100

    viewmodel added

--- app/src/main/java/com/polish/leadershipdevotional/di/RepositoryModule.kt
+++ app/src/main/java/com/polish/leadershipdevotional/di/RepositoryModule.kt
@@ -1,7 +1,22 @@
 package com.polish.leadershipdevotional.di
 
+import com.polish.leadershipdevotional.model.dao.LeadershipDevotionalDao
+import com.polish.leadershipdevotional.repository.ILeadershipDevotionalRepository
+import com.polish.leadershipdevotional.repository.LeadershipDevotionalRepositoryImpl
+import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
-@InstallIn
+@Module
+@InstallIn(SingletonComponent::class)
 object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideLeadershipRepository(leadershipDevotionalDao: LeadershipDevotionalDao): ILeadershipDevotionalRepository {
+        return LeadershipDevotionalRepositoryImpl(leadershipDevotionalDao)
+    }
+
 }
---
+++ app/src/main/java/com/polish/leadershipdevotional/viewmodel/LeadershipDevotionalViewModel.kt
@@ -0,0 +1,4 @@
+package com.polish.leadershipdevotional.viewmodel
+
+class LeadershipDevotionalViewModel {
+}

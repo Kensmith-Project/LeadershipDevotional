be649d16eab7064246e40e0e3a44b3794333445f
Osehi <osehiaseehilen@gmail.com>
2021-06-02 12:29:30 +0100

    detailedDevotionalActivity added

---
+++ app/src/main/java/com/polish/leadershipdevotional/DetailedDevotionalActivity.kt
@@ -0,0 +1,11 @@
+package com.polish.leadershipdevotional
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailedDevotionalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailed_devotional)
+    }
+}
